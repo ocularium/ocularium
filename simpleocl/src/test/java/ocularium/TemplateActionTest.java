5239350b9ff0e1e00b964db8445a843d6c77a4d9
Marco Mangan <marco.mangan@gmail.com>
2016-08-13 08:06:05 -0300

    #4

---
+++ simpleocl/src/test/java/ocularium/TemplateActionTest.java
@@ -0,0 +1,17 @@
+package ocularium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TemplateActionTest {
+	
+	@Test
+	public void testname() throws Exception {
+		
+	}
+
+}
+
+
