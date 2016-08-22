68b4b0d7d9756a78dfbf5d038fa675505f018816
Marco Mangan <marco.mangan@gmail.com>
2016-08-21 21:40:45 -0300

    #9

---
+++ simpleocl/src/main/java/ocularium/OculariumPanel.java
@@ -0,0 +1,21 @@
+package ocularium;
+
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+/**
+ * 
+ * @author marco.mangan@pucrs.br
+ *
+ */
+public class OculariumPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1003621475221495550L;
+	
+	private JTable table;
+
+	
+}
