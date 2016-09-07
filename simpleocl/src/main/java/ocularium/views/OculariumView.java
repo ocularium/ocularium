60e9db23d6f71e50d39357bad50dee34b0acb20c
Marco Mangan <marco.mangan@gmail.com>
2016-09-07 15:14:35 -0300

    #69 R&L levels is not working

--- simpleocl/src/main/java/ocularium/views/OculariumView.java
+++ simpleocl/src/main/java/ocularium/views/OculariumView.java
@@ -31,27 +31,27 @@ import java.util.List;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import com.change_vision.jude.api.inf.AstahAPI;
 import com.change_vision.jude.api.inf.exception.ProjectNotFoundException;
 import com.change_vision.jude.api.inf.model.IConstraint;
+import com.change_vision.jude.api.inf.model.IElement;
 import com.change_vision.jude.api.inf.model.IModel;
 import com.change_vision.jude.api.inf.project.ProjectAccessor;
 import com.change_vision.jude.api.inf.project.ProjectEvent;
 import com.change_vision.jude.api.inf.project.ProjectEventListener;
 import com.change_vision.jude.api.inf.ui.IPluginExtraTabView;
 import com.change_vision.jude.api.inf.ui.ISelectionListener;
+import com.change_vision.jude.api.inf.view.IProjectViewManager;
+import com.change_vision.jude.api.inf.view.IViewManager;
 
 import ocularium.internal.OculariumFacade;
 
 /**
  * Constraint table panel.
  * 
- * Ocularium main user interface is a Astah API extra tab view. This panel has a
- * single JTable component and the corresponding table model. There are two
+ * Ocularium main user interface is an Astah API extra tab view. This panel has
+ * a single JTable component and the corresponding table model. There are two
  * listeners: a row listener and a project event listener,
  * 
  * The row listener is notified when the use selects a row in the table view.
@@ -79,22 +79,30 @@ public class OculariumView extends JPanel implements IPluginExtraTabView {
 	 * @author marco.mangan@gmail.com
 	 *
 	 */
-	private class RowListener implements ListSelectionListener {
-
-		@Override
-		public void valueChanged(ListSelectionEvent e) {
-			if (e.getValueIsAdjusting()) {
-				return;
+	private final class OculariumMouseListener extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2) {
+				JTable target = (JTable) e.getSource();
+				int row = target.getSelectedRow();
+				if (row != -1) {
+					System.out.println("==> MouseAdapter::mouseClicked");
+					System.out.println(row);
+					IConstraint c = dm.getConstraintAtRow(row);
+					System.out.println(c);
+					// https://github.com/ChangeVision/astah-sample-taggedvalue-plugin/blob/master/src/main/java/com/change_vision/astah/AddTaggedValueAction.java
+					try {
+						AstahAPI api = AstahAPI.getAstahAPI();
+						ProjectAccessor projectAccessor = api.getProjectAccessor();
+						IViewManager viewManager = projectAccessor.getViewManager();
+						IProjectViewManager projectViewManager = viewManager.getProjectViewManager();
+						IElement element = c.getConstrainedElement()[0];
+						projectViewManager.showInStructureTree(element);
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
 			}
-			int row = e.getFirstIndex();
-			System.out.println("==> RowListener::valueChanged");
-			System.out.println(row);
-			System.out.println(e);
-
-			IConstraint c = dm.getConstraintAtRow(row);
-			System.out.println(c);
 		}
-
 	}
 
 	/**
@@ -136,8 +144,9 @@ public class OculariumView extends JPanel implements IPluginExtraTabView {
 	private OclTableModel dm;
 
 	/**
+	 * Testing support method.
 	 * 
-	 * @return
+	 * @return current table model.
 	 */
 	public OclTableModel getModel() {
 		return dm;
@@ -148,42 +157,16 @@ public class OculariumView extends JPanel implements IPluginExtraTabView {
 	 */
 	public OculariumView() {
 		setLayout(new GridLayout(1, 0));
-
 		dm = new OclTableModel();
 		final JTable table = new JTable(dm);
-		// table.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		table.setFillsViewportHeight(true);
 
 		JScrollPane scrollPane = new JScrollPane(table);
 
 		add(scrollPane);
-		//
 
-		// addListener on activate?
 		addProjectEventListener();
-		table.getSelectionModel().addListSelectionListener(new RowListener());
-		// table.setRowSelectionAllowed(true);
-		ListSelectionModel listMod = table.getSelectionModel();
-		listMod.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		// listMod.addListSelectionListener(this);
-
-		// System.out.println("Adding listeners");
-		 table.addMouseListener(new MouseAdapter() {
-		 public void mouseClicked(MouseEvent e) {
-		 if (e.getClickCount() == 2) {
-		 JTable target = (JTable) e.getSource();
-		 int row = target.getSelectedRow();
-		 // int column = target.getSelectedColumn();
-		 // do some action
-		 if (row != -1) {
-		 System.out.println("==> MouseAdapter::mouseClicked");
-		 System.out.println(row);
-			IConstraint c = dm.getConstraintAtRow(row);
-			System.out.println(c);
-		 }
-		 }
-		 }
-		 });
+		table.addMouseListener(new OculariumMouseListener());
 	}
 
 	/**
@@ -200,7 +183,6 @@ public class OculariumView extends JPanel implements IPluginExtraTabView {
 
 	@Override
 	public void addSelectionListener(ISelectionListener listener) {
-		// TODO: System.out.printf("Teste%n");
 	}
 
 	@Override
@@ -236,7 +218,6 @@ public class OculariumView extends JPanel implements IPluginExtraTabView {
 	 * Load all constraints into table model
 	 */
 	private void refresh() {
-		// TODO: could implement incremental refresh
 		AstahAPI api;
 		try {
 			api = AstahAPI.getAstahAPI();
